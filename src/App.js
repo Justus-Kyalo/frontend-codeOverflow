9889786cee87265199a869803d33bd2489998630
antonyosebe <osebe.anto@gmail.com>
2023-04-18 13:47:51 +0300

    include the splash/homepage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Navbar from "./components/Nav";
+import SplashPage from "./components/Splash";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div id="page-content">
+        <Switch>
+        <Route exact path="/">
+              <SplashPage />
+        </Route>
+
+      </Switch>
+      </div>
     </>
   );
 }
