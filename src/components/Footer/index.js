59aec67c424295f851d2285ca3673dec404a1afa
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-18 15:34:21 +0300

    added footet component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './index.css';
+
+const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0, 
+            behavior: 'smooth'
+          });
+    }
+    return (
+        <footer>
+            <div className="footer-container">
+                <div className="footer-left">
+                    <div className="navigation">
+                        <a className="main" onClick={scrollToTop}>
+                            codeOverflow
+                        </a>
+                        <ul>
+                            <Link to="/questions">
+                                <li>FAQ</li>
+                            </Link>
+                            <a href="https://stackoverflow.com/help" target="_blank">
+                                <li>Help</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer-right">
+                    <div className="navigation">
+                        <ul>
+                            <a href="" target="_blank">
+                                <li>Github</li>
+                            </a>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
