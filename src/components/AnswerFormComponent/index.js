fefbf53093b9cc96e43442503a8ed3f596a8245b
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-04-18 14:07:01 +0300

    added answer form component

---
+++ src/components/AnswerFormComponent/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAnswers } from "../../store/answers";
+import { fetchAllQuestions } from "../../store/questions";
+
+import './index.css'
+
+const AnswerForm = ({questionId}) => {
+    const dispatch = useDispatch();
+    const [description, setDescription] = useState();
+    const author = useSelector(state => state.session.user);   
+
+    const handleClick = async e => {
+        e.preventDefault();
+        const answer = {answerer_id: author.id, description: description, question_id: questionId};
+        const res = await csrfFetch('/api/answers', {
+            method: 'POST',
+            body: JSON.stringify(answer)
+        });
+        if (res.ok){
+            await res.json();
+            dispatch(fetchAnswers());
+            dispatch(fetchAllQuestions())
+            setDescription("");
+        } 
+    };
+
+    return(
+        <form className="answer-form-container">
+            <h2 id="your-answer">Your Answer</h2>
+            <textarea onChange={e => setDescription(e.target.value)} id="" cols="30" rows="10" value={description}></textarea>
+            <button className="login-button post-answer-button" onClick={handleClick}>Post Your Answer</button>
+        </form>
+    )
+};
+
+export default AnswerForm;
